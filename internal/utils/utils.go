4cbb88e35a271b34f00d5a47e95186eb898b9710
Arthur Wallendorff <howdy@wallenart.dev>
2024-03-20 23:56:16 +0100

    feat: add struct pretty formatter

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,11 +1,25 @@
 package utils
 
 import (
+	"fmt"
 	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func RootDir() string {
 	_, file, _, _ := runtime.Caller(0)
 	return filepath.Join(filepath.Dir(file), "../..")
+}
+
+// Pretty format struct
+func FormatStruct(s interface{}, msg ...string) string {
+	return strings.Join(
+		msg,
+		" ",
+	) + "\n" + strings.ReplaceAll(
+		fmt.Sprintf("%+v", s),
+		" ",
+		"\n",
+	)
 }
