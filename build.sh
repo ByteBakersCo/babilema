01e821a4417636be2d0ca1365aedd47a45f8c3bf
Arthur Wallendorff <howdy@wallenart.dev>
2024-03-25 14:01:22 +0100

    feat: better build script :)

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Building..."
+echo "Downloading dependencies..."
+go mod download && go mod verify
+
+echo "Running tests..."
+go test -v ./...
+
+echo "Building binaries..."
 go build -v -o babilema ./cmd/babilema/main.go
 
 echo "Successfully built the project, run it by executing ./babilema"
