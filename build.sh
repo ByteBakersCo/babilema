c231f76ab3b590e74d90ba13c447d68f690c3c9d
Arthur Wallendorff <howdy@wallenart.dev>
2024-06-22 11:56:45 +0200

    chore: add coverage feedback in test command

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Downloading dependencies..."
 go mod download && go mod verify
 
 echo "Running tests..."
-go test -v ./...
+go test -v --cover ./...
 
 echo "Building binaries..."
 go build -v -o babilema ./cmd/babilema/main.go
