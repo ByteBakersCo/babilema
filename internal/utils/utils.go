2c1aafd53525ed753823f8e8f00b852c16447509
Arthur Wallendorff <howdy@wallenart.dev>
2024-06-24 22:16:08 +0200

    chore: use a more descriptive name for fn

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -60,7 +60,7 @@ func IsCommandAvailable(cmd string) bool {
 	return command.Run() == nil
 }
 
-func IsFileExists(filename string) bool {
+func IsFileAndExists(filename string) bool {
 	info, err := os.Stat(filename)
 	if os.IsNotExist(err) {
 		return false
